9bdcd30a545f5945e22298e3605a16ba37e808c3
Gary Baldrich <gbaldrich16@gmail.com>
2022-08-20 22:34:37 -0500

    creating express app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const app = require('./server')
+
+require('./server')
+const port = 3000
+
+
+app.listen(port, () =>{
+    console.log(`Listening on port ${port}`)
+})
---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Initializations
+module.exports = app
+
+
+// Settings
+
+
+//Middlewares
